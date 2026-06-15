483f7e35d7f9eff42a18744a8d7c0e1c778d15aa
Have Samuel <samiehave9@gmail.com>
2026-06-15 05:34:28 +0300

    Creating validations model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  # Validations. Title Length, or has to exist, and body length or has to exist
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
