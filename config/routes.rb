3cbc4a588da188722a0c5217f73e1bddab8482da
Have Samuel <samiehave9@gmail.com>
2026-06-17 05:02:50 +0300

    Adding more routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "posts", to: "posts#index", as: :posts
   get "posts/new", to: "posts#new"
   get "posts/:id/edit", to: "posts#edit", as: :edit_post
+  patch "posts/:id", to: "posts#update"
 
   get "posts/:id", to: "posts#show", as: :post
   post "posts", to: "posts#create"
